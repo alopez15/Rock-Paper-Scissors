85c78110531c347bf36b87af8dfe1d9b4ed76c7d
Alfred <fredzterlopez0530@gmail.com>
2021-11-24 06:26:24 -0500

    finished UI

--- app.js
+++ app.js
@@ -1,108 +1,191 @@
-  let  computerPlay = () => {
+const  computerPlay = () => {
+
     let randomNum = Math.floor(Math.random() * 3);
     let compPlay;
 
     switch (randomNum) {
         case 0:
-            compPlay = "Rock";
+            compPlay = 'Rock';
             break;
 
         case 1:
-            compPlay = "Paper";
+            compPlay = 'Paper';
             break;
 
         case 2:
-            compPlay = "Scissors";
+            compPlay = 'Scissors';
     }
+    console.log(compPlay)
     return compPlay;
 };
 
-let result;
+const playRound = (computerSelection, playerSelection) => {
 
- let playRound = (computerSelection, playerSelection) => {
+    if (playerSelection === 'Rock' && computerSelection === 'Rock') {
+        return result = null;
 
-    if (playerSelection === "Rock" && computerSelection === "Rock") {
-        result = null;
-        return `${playerSelection} vs ${computerSelection}, it's a Draw!`;
+    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        return result = true;;
 
-    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        result = true;
-        return `${playerSelection} beats ${computerSelection}, You Won!`;
+    } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+        return result = false;
 
-    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
-        result = false;
-        return `${computerSelection} beats ${playerSelection}, You Lose!`;
+    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+        return result = true;
 
-    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-        result = true;
-        return `${playerSelection} beats ${computerSelection}, You Won!`;
+    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        return result = false;
 
-    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-        result = false;
-        return `${computerSelection} beats ${playerSelection}, You Lose!`;
+    } else if (playerSelection === 'Paper' && computerSelection === 'Paper') { 
+        return result = null;
 
-    } else if (playerSelection === "Paper" && computerSelection === "Paper") {
-        result = null
-        return `${computerSelection} vs ${playerSelection}, It's a Draw!`;
+    }else if (playerSelection === 'Scissors' && computerSelection === 'Scissors') {
+        return result = null;
 
-    }else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
-        result = null
-        return `${computerSelection} vs ${playerSelection}, It's a Draw!`;
+    }else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        return result = false;
 
-    }else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-        result = false;
-        return `${computerSelection} beats ${playerSelection}, You Lose!`;
-
-    }else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-        result = true;
-        return `${playerSelection} beats ${computerSelection}, You Won!`;
+    }else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        return result = true;
 }
 };
 
-const round = 5;
- 
-let game = () => {
+const displayImg = (compPlay) => {
 
-    let playerScore = 0, computerScore = 0, gameRound = 0, flag = 0;
-    let playerSelection, newPlayerSelection, computerSelection, firstLetter, newFirstLetter, slicedWord;
+    if(compPlay === 'Scissors'){
+        computerChoice.src = './img/scissors2.png'
 
-    for(let i = 0; i < round; i++){
-    
-        while (flag == 0){
-            playerSelection = prompt('Enter Rock, Paper, or Scissors');
+    } else if(compPlay === 'Rock'){
+        computerChoice.src = './img/rock2.jpg'
 
-            //Takes first letter and convert it to uppercase, then 
-            //Then concatenate with the rest of the word after slicing
-            //The first letter and converting it ti lowecase
-            newPlayerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();  
+    } else if(compPlay === 'Paper'){
+        computerChoice.src = './img/paper2.png'
 
-        if(newPlayerSelection != 'Rock' && newPlayerSelection != 'Scissors' && newPlayerSelection != 'Paper'){
-            alert('Invalid Input, Try Again!')
-        
-        } else {
-            flag = 1;
-
-        }
-        }   
-
-    gameRound++;
-    computerSelection = computerPlay();
-    
-    console.log(`Round ${gameRound} \nComputer played ${computerSelection}`)
-    console.log(playRound(computerSelection, newPlayerSelection))
-
-    if (result === true){
-        playerScore++
-
-    }else if(result === false){
-        computerScore++
-    }
-
-    console.log(`Player Score: ${playerScore} \nComputer Score: ${computerScore}`)
-    flag = 0;
-    }
-
-    console.log(`Game Over!`)
+    };
 }
 
-game();
+const printResult = (result) => {
+    if (result === null){
+        gameResult.textContent = 'It\'s a Draw!';
+        
+
+    } else if (result === true ){
+        gameResult.textContent = 'Player Won!';
+        
+        
+    } else if( result === false ){
+        gameResult.textContent = 'Computer Won!'
+        
+
+    }
+}
+
+const playerChoice = document.querySelector('#player-img')
+const computerChoice = document.querySelector('#computer-img')
+const scissors = document.querySelector('#scissors-btn')
+const rock = document.querySelector('#rock-btn')
+const paper = document.querySelector('#paper-btn')
+const gameResult = document.querySelector('#game-result')
+const playerScoreID = document.querySelector('#player-score')
+const computerScoreID = document.querySelector('#computer-score')
+const buttons = document.querySelector('#btns')
+const animation = document.querySelector('.animation')
+
+let playerScore = 0, computerScore = 0;
+
+scissors.addEventListener('click', (e) => {
+
+    let compPlay = computerPlay()
+    let result = playRound(compPlay, 'Scissors')
+
+    // computerChoice.className += animation' 
+    // figure out how to add animation on click
+
+    playerChoice.src = './img/scissors(1).png'
+
+    displayImg(compPlay);
+    printResult(result);
+
+    if (result === true){
+        playerScore++;
+
+    } else if(result === false){
+        computerScore++;
+
+    }
+
+    playerScoreID.textContent = playerScore;
+    computerScoreID.textContent = computerScore;
+    endGame(playerScore, computerScore);
+
+});
+
+rock.addEventListener('click', (e) => {
+
+    let compPlay = computerPlay();
+    let result = playRound(compPlay, 'Rock');
+    
+    displayImg(compPlay);
+    printResult(result);
+
+    playerChoice.src = './img/rock.jpg';
+
+    if (result === true){
+        playerScore++;
+
+    } else if(result === false){
+        computerScore++;
+
+    }
+
+    playerScoreID.textContent = playerScore;
+    computerScoreID.textContent = computerScore;
+    endGame(playerScore, computerScore);
+
+});
+
+paper.addEventListener('click', (e) => {
+
+    let compPlay = computerPlay();
+    let result = playRound(compPlay, 'Paper');   
+    
+    displayImg(compPlay);
+    printResult(result);
+
+    playerChoice.src = './img/paper(1).png';
+
+    if (result === true){
+        playerScore++;
+
+    } else if(result === false){
+        computerScore++;
+
+    }
+
+    playerScoreID.textContent = playerScore;
+    computerScoreID.textContent = computerScore;
+    endGame(playerScore, computerScore)
+
+});
+
+const endGame = (playerScore, computerScore) => {
+    
+    if( playerScore === 5 && computerScore < 5){
+        gameResult.textContent = 'Game Over, Player Wins!';
+        buttons.style.display = 'none';
+        gameFlag = 1;
+
+    } else if ( playerScore === 5 && computerScore === 5 ){
+        gameResult.textContent = 'Game Over, It\'s a Draw!';
+        buttons.style.display = 'none';
+        gameFlag = 1;
+
+    } else if (playerScore < 5 && computerScore === 5){
+        gameResult.textContent = 'Game Over, Computer Wins!';
+        buttons.style.display = 'none';
+        gameFlag = 1;
+    }
+       
+}
+
+
